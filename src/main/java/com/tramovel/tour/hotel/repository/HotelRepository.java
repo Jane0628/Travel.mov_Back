2e0514d7c10a47e74cd13a806e843ec7220199a9
dongjun <sdj799@naver.com>
2023-07-26 12:05:22 +0900

    23.07.26 | dj | 호텔 데이터 넘기기 완료

--- src/main/java/com/tramovel/tour/hotel/repository/HotelRepository.java
+++ src/main/java/com/tramovel/tour/hotel/repository/HotelRepository.java
@@ -3,10 +3,11 @@ package com.tramovel.tour.hotel.repository;
 import com.tramovel.tour.hotel.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface HotelRepository extends JpaRepository<Hotel, String> {
-  @Query(value = "SELECT * FROM hotel WHERE address LIKE '%:address%'" , nativeQuery = true)
-  List<Hotel> findByAddress(String address);
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+  @Query(value = "SELECT * FROM hotel WHERE address LIKE %:address%" , nativeQuery = true)
+  List<Hotel> findByAddress(@Param("address") String address);
 }
